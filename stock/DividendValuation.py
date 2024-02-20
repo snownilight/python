71e3f55278f3a7057b9f68611ba76dcd84eccea9
Snownilight <a510420a@gmail.com>
2024-02-20 11:57:05 +0800

    [ST-10] - 完成10年平均股利估價

--- stock/DividendValuation.py
+++ stock/DividendValuation.py
@@ -8,49 +8,34 @@ stock = yf.Ticker(stockid + '.TW')
 # stock = yf.Ticker('2330.TW')
 
 dividend = round(stock.dividends, 1)
-data = pd.DataFrame(dividend)
-
-data = data.reset_index()
-data.columns = ['日期', '股利']
-
-# for item in dividend:
-    
-
-# for item2 in dividend.index:
-#     print(item2)
 
 hash = {key: value for key, value in zip(dividend.index, dividend)}
 rightHash = {}
 nowYear = ''
 sameYearDiv = 0
 
-## FIXME:last year doesn't append to righthash
-
+# add dividends from the same year
 for k, v in hash.items():
-    if nowYear != '' and nowYear != k.strftime('%Y'):
-        rightHash[nowYear] = sameYearDiv
+    if nowYear != k.strftime('%Y'):
         sameYearDiv = 0
+        nowYear = k.strftime('%Y')
 
     sameYearDiv += v
-    nowYear = k.strftime('%Y')
+    rightHash[nowYear] = sameYearDiv
 
+tenYearTotal = 0
+for i in range(1, 11):
+    tenYearTotal += list(rightHash.values())[-i]
 
-print(rightHash)
+average = tenYearTotal/10
 
+print(f"平均股利：{average}")
 
+safe = 0.8
+cheap = round(average * 15 * safe, 2)
+middle = round(average * 20 * safe, 2)
+expensive = round(average * 30 * safe, 2)
 
-
-# print(data)
-
-# average = data['股利'].tail(10).mean()
-
-# print(f"平均股利：{average}")
-
-# safe = 0.8
-# cheap = round(average * 15 * safe, 2)
-# middle = round(average * 20 * safe, 2)
-# expensive = round(average * 30 * safe, 2)
-
-# print(f"便宜價：{cheap}")
-# print(f"中間價：{middle}")
-# print(f"昂貴價：{expensive}")
+print(f"便宜價：{cheap}")
+print(f"中間價：{middle}")
+print(f"昂貴價：{expensive}")
